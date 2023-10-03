271e919a328b4c50a2d79911251e9cc87c546e59
ZoeMesaros <zoe.mesaros@hotmail.com>
2023-10-03 15:15:55 +0200

    fixes

--- themes/uppgift-shop/front-page.php
+++ themes/uppgift-shop/front-page.php
@@ -6,21 +6,28 @@ $heroImage = get_field('hero_image')
 
 <?php get_header(); ?>
 
-<section class="px-5 py-6 py-xxl-10 hcf-bp-center hcf-bs-cover hcf-overlay hcf-transform"
-    style="background-image: url('<?php echo esc_url($heroImage); ?>');">
+
+<?php if ($heroImage): ?>
+    <section class="px-5 py-6 py-xxl-10 hcf-bp-center hcf-bs-cover hcf-overlay hcf-transform"
+        style="background-image: url('<?php echo esc_url($heroImage); ?>');">
+    <?php endif; ?>
     <div class="container">
         <div class="row justify-content-md-left">
             <div class="col-12 col-md-11 col-lg-9 col-xl-7 col-xxl-6 text-left text-white">
-                <h3 class="display-5 mb-3 me-5">
-                    <?php echo $pageTitle ?>
-                </h3>
+                <?php if ($pageTitle): ?>
+                    <h3 class="display-5 mb-3 me-5">
+                        <?php echo $pageTitle ?>
+                    </h3>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 </section>
-<p class="lead mb-5">
-    <?php echo $description ?>
-</p>
+<?php if ($description): ?>
+    <p class="lead mb-5">
+        <?php echo $description ?>
+    </p>
+<?php endif; ?>
 
 
 

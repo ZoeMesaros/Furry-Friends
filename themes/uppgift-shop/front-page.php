85c38b7dd1d5eccda53c3cc71127f431b707f16e
ZoeMesaros <zoe.mesaros@hotmail.com>
2023-10-04 14:13:36 +0200

    frontpage

--- themes/uppgift-shop/front-page.php
+++ themes/uppgift-shop/front-page.php
@@ -1,7 +1,8 @@
 <?php
 $pageTitle = get_field('page_title');
 $description = get_field('description');
-$heroImage = get_field('hero_image')
+$heroImage = get_field('hero_image');
+$lProducts = get_field('latest_products')
     ?>
 
 <?php get_header(); ?>
@@ -28,35 +29,12 @@ $heroImage = get_field('hero_image')
         <?php echo $description ?>
     </p>
 <?php endif; ?>
-<?php
-$args = array(
-    'post_type' => 'product',
-    'stock' => 1,
-    'posts_per_page' => 4,
-    'orderby' => 'date',
-    'order' => 'DESC'
-);
-$loop = new WP_Query($args);
-while ($loop->have_posts()):
-    $loop->the_post();
-    global $product; ?>
-    <div class="span3">
-        <a id="id-<?php the_id(); ?>" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-            <?php if (has_post_thumbnail($loop->post->ID))
-                echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog');
-            else
-                echo '<img src="' . woocommerce_placeholder_img_src() . '" alt="My Image Placeholder" width="65px" height="115px" />'; ?>
-            <h3>
-                <?php the_title(); ?>
-            </h3>
-            <span class="price">
-                <?php echo $product->get_price_html(); ?>
-            </span>
-        </a>
-        <?php woocommerce_template_loop_add_to_cart($loop->post, $product); ?>
-    </div><!-- /span3 -->
-<?php endwhile; ?>
-<?php wp_reset_query(); ?>
+<?php if ($lProducts): ?>
+    <h2>
+        <?php echo $lProducts ?>
+    </h2>
+<?php endif; ?>
+<?php echo do_shortcode("[products limit='4' columns='4' orderby='id' order='DESC' visibility='visible']"); ?>
 
 
 <?php get_footer(); ?>
--- themes/uppgift-shop/functions.php
+++ themes/uppgift-shop/functions.php
@@ -120,19 +120,3 @@ function my_wp_nav_menu_items($items, $args)
     return $items;
 
 }
-
-
-
-
-
-add_action('woocommerce_before_shop_loop_item_title', 'quadlayers_new_product_badge', 3);
-
-function quadlayers_new_product_badge()
-{
-    global $product;
-    $newness_days = 10;
-    $created = strtotime($product->get_date_created());
-    if ((time() - (60 * 60 * 24 * $newness_days)) < $created) {
-        echo '<span class="itsnew onsale">' . esc_html__('New!', 'woocommerce') . '</span>';
-    }
-}

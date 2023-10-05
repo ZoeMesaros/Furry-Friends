4bf2a97cb463366264ad1458e4360dcdde1fc81e
NatasaIc <iconomou.ng@gmail.com>
2023-10-05 07:15:10 +0200

    adding card styling for posts

--- themes/uppgift-shop/content-post.php
+++ themes/uppgift-shop/content-post.php
@@ -8,31 +8,35 @@ $postCategory = $args['postCategory'];
 
 <?php get_header(); ?>
 
-<div class="container text-center">
-    <?php if($postCategory): ?>
-    <div id="category-box" class="mb-3 mt-5">
-    <h4 id="category"><?php echo $postCategory ?></h4>
-    </div>
+<div class="container text-start">
+    <?php if ($postCategory): ?>
+        <div id="category-box" class="mb-3 mt-5">
+            <h4 id="category"><?php echo $postCategory ?></h4>
+        </div>
     <?php endif; ?>
 
-<h1 class="mb-5 mt-5 d-grid gap-5"><?php echo get_the_title(); ?></h1>
+    <div class="card shadow mb-5 mt-3">
+        <div class="card-body">
+            <h1 class="mb-5 d-grid gap-5"><?php echo get_the_title(); ?></h1>
 
-<section class="row justify-content-center align-items-center">
-<?php if ($postImg): ?>
-    <div class="col-md-6">
-        <img src="<?php echo esc_url($postImg); ?>" class="img-fluid" alt="Nyhets Produkt">
+            <section class="row justify-content-center align-items-center">
+                <?php if ($postImg): ?>
+                    <div class="col-md-6">
+                        <img src="<?php echo esc_url($postImg); ?>" class="img-fluid" alt="Nyhets Produkt">
+                    </div>
+                <?php endif; ?>
+
+                <div class="col-md-6">
+                    <?php if ($postDescription): ?>
+                        <p class="description"><?php echo $postDescription ?></p>
+                    <?php endif; ?>
+
+                    <?php if ($postLink): ?>
+                        <a href="<?php echo esc_url($postLink); ?>" class="btn btn-primary mb-3 mt-5">Köp!</a>
+                    <?php endif; ?>
+                </div>
+            </section>
+        </div>
     </div>
-<?php endif; ?>
-
-<div class="col-md-6">
-<?php if($postDescription): ?>
-    <p class='description'><?php echo $postDescription ?></p>
-<?php endif; ?>
-
-<?php if($postLink): ?>
-    <a href="<?php echo esc_url($postLink); ?>" class="btn btn-primary mb-3 mt-5">Köp!</a>
-    <?php endif; ?>
-     </div>
-</section>
 </div>
 <?php get_footer(); ?>

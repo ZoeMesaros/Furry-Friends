f36cbf403792378f6874ab7e2a153725b56dad3a
NatasaIc <iconomou.ng@gmail.com>
2023-10-12 08:04:15 +0200

    contact styling

--- themes/uppgift-shop/page-kontakt.php
+++ themes/uppgift-shop/page-kontakt.php
@@ -1,15 +1,16 @@
 <?php
-$contactTitle = get_field('contact_title', 'option');
+$contactTitle = get_field('contact_title');
 ?>
 
 <?php get_header(); ?>
-
-<section>
-    <?php if ($contactTitle): ?> 
-        <h1><?php echo esc_html($contactTitle); ?></h1>
+<?php if ($contactTitle): ?> 
+        <h1 style="text-align: center; margin: 6%;"><?php echo esc_html($contactTitle); ?></h1>
     <?php endif; ?>
+<section class="contact-section" style="display: flex; margin: 10%;">
 
-    <?php echo do_shortcode('[contact-form-7 id="ac0e918" title="Contact form 1"]'); ?>
+    <div style="width: 40%; margin-left: 5%"> 
+        <?php echo do_shortcode('[contact-form-7 id="ac0e918" title="Contact form 1"]'); ?>
+    </div>
 
     <?php
     $args = array(
@@ -19,40 +20,40 @@ $contactTitle = get_field('contact_title', 'option');
 
     $stores = get_posts($args);
 
-    if ($stores) :
-        echo '<div class="container store-list border p-4 mt-5">';
-        echo '<h3 class="mb-4">Butiker & Öppettider</h3>';
-        foreach ($stores as $store) {
-            $storeName         = get_field('store_name', $store->ID);
-            $OpenHoursWeek     = get_field('store_hours_weekdays', $store->ID);
-            $OpenHoursWeekends = get_field('store_hours_weekends', $store->ID);
-            $storeAddress      = get_field('store_address', $store->ID);
-            $storePhone        = get_field('store_phone', $store->ID);
-            $storeEmail        = get_field('store_email', $store->ID);
-
-            // Output store information as needed
-            echo '<div class="row store-item mb-4 border-bottom pb-3">';
-            echo '<div class="col-md-6">';
-            echo '<p class="mb-2"><strong>' . esc_html($storeName) . '</strong></p>';
-            if ($OpenHoursWeek) {
-                echo '<p>Vardagar: ' . esc_html($OpenHoursWeek) . '</p>';
-            }
-            if ($OpenHoursWeekends) {
-                echo '<p>Helger: ' . esc_html($OpenHoursWeekends) . '</p>';
-            }
-            echo '</div>'; 
-            echo '<div class="col-md-6">';
-            echo '<p>Adress: ' . esc_html($storeAddress) . '</p>';
-            echo '<p>Tel: ' . esc_html($storePhone) . '</p>';
-            echo '<p>E-post: ' . esc_html($storeEmail) . '</p>';
-            echo '</div>';
-            echo '</div>';
-        }
-        echo '</div>';
-    else :
-        echo '<div class="container"><p class="alert alert-warning">No stores found.</p></div>';
-    endif;
-    ?>
+    if ($stores) : ?>
+        <div class="container store-list" style="width: 40%; margin-right: 5%">
+            <h3 class="mb-4">Butiker & Öppettider</h3>
+            <?php foreach ($stores as $store) :
+                $storeName         = get_field('store_name', $store->ID);
+                $openHoursWeek     = get_field('store_hours_weekdays', $store->ID);
+                $openHoursWeekends = get_field('store_hours_weekends', $store->ID);
+                $storeAddress      = get_field('store_address', $store->ID);
+                $storePhone        = get_field('store_phone', $store->ID);
+                $storeEmail        = get_field('store_email', $store->ID);
+                $storeImage        = get_field('store_image', $store->ID);
+            ?>
+                <div class="row store-item mb-4 border-bottom pb-3">
+                    <div class="col-md-6">
+                        <p class="mb-2"><strong><?php echo esc_html($storeName); ?></strong></p>
+                        <?php if ($openHoursWeek): ?>
+                            <p>Vardagar: <?php echo esc_html($openHoursWeek); ?></p>
+                        <?php endif; ?>
+                        <?php if ($openHoursWeekends): ?>
+                            <p>Helger: <?php echo esc_html($openHoursWeekends); ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="col-md-6">
+                        <p>Adress: <?php echo esc_html($storeAddress); ?></p>
+                        <p>Tel: <?php echo esc_html($storePhone); ?></p>
+                        <p>E-post: <?php echo esc_html($storeEmail); ?></p>
+                    </div>
+                    <img style="width: 70%;" src="<?php echo esc_url($storeImage); ?>" alt="<?php echo esc_attr($storeName); ?>"/>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else : ?>
+        <div class="container"><p class="alert alert-warning">No stores found.</p></div>
+    <?php endif; ?>
 </section>
 
 <?php get_footer(); ?>
